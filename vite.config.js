7f42d85aa2d157b877d6541a7aae82aa70c52a75
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-10-04 14:23:00 +0300

    WindiCSS setup

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@
 /** @type {import('vite').UserConfig} */
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
   // omit
-  plugins: [mkcert()],
+  plugins: [mkcert(),
+    WindiCSS({
+      scan: {
+        dirs: ['.', './**/*.html'], // all files in the cwd
+        fileExtensions: ['vue', 'js', 'ts', 'html'], // also enabled scanning for js/ts
+      },
+    }
+  )],
   server: {
     port: 8089,
     cors: true,
---
+++ windi.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  /* configurations... */
+  extract: {
+    include: ['src/**/*.{vue,html,js,jsx,tsx}', './**/*.html', 'index.html'],
+    exclude: ['node_modules', '.git'],
+  },
+})
