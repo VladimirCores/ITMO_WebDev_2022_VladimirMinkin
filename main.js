8f67a98d94a9f30f375cbba6e78c8c1a57ec1a38
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-10-13 22:12:18 +0300

    Todo validation first step

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const domInpTodoTitle = document.getElementById("inpTodoTitle");
 const domBtnCreateTodo = document.getElementById("btnCreateTodo");
 const domListOfTodos = document.getElementById("listOfTodos");
 
+domBtnCreateTodo.addEventListener("click", onBtnCreateTodoClick);
+
 class TodoVO {
   constructor(id, title, date = new Date()) {
     this.id = id;
@@ -13,24 +15,42 @@ class TodoVO {
 
 const listOfTodos = [];
 
-domInpTodoTitle.value = "Todo text";
-
-domBtnCreateTodo.addEventListener("click", (e) => {
-  console.log("> domBtnCreateTodo -> handle(click)", e);
+function onBtnCreateTodoClick(event) {
+  console.log("> domBtnCreateTodo -> handle(click)", event);
   const todoTitleValueFromDomInput = domInpTodoTitle.value;
-  console.log(
-    "> domBtnCreateTodo -> todoInputTitleValue:",
-    todoTitleValueFromDomInput
-  );
+  console.log("> domBtnCreateTodo -> todoInputTitleValue:", todoTitleValueFromDomInput);
 
-  const todoId = Date.now().toString();
-  const todoVO = new TodoVO(todoId, todoTitleValueFromDomInput);
+  const canCreateTodo = validateTodoInputTitleValue(todoTitleValueFromDomInput);
 
-  listOfTodos.push(todoVO);
-
-  let output = "";
-  for (let index in listOfTodos) {
-    output += `<li>${listOfTodos[index].title}</li>`;
+  if (canCreateTodo) {
+    const todoVO = createTodoVO(todoTitleValueFromDomInput);
+    listOfTodos.push(todoVO);
+    let output = "";
+    for (let index in listOfTodos) {
+      output += `<li>${listOfTodos[index].title}</li>`;
+    }
+    domListOfTodos.innerHTML = output;
   }
-  domListOfTodos.innerHTML = output;
-});
+}
+
+function validateTodoInputTitleValue(value) {
+  const isInputValueString = typeof value === 'string';
+  const isInputValeNotNumber = isNaN(parseInt(value));
+
+  const result =
+        isInputValueString
+    &&  isInputValeNotNumber
+    &&  value.length > 0;
+
+  console.log('> validateTodoInputTitleValue -> result', {
+    result,
+    isInputValueString,
+    isInputValeNotNumber
+  });
+  return result;
+}
+
+function createTodoVO(title) {
+  const todoId = Date.now().toString();
+  return new TodoVO(todoId, title);
+}
