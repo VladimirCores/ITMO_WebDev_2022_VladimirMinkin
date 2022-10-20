df9d83768065abf3b3cb9f73d3f93c4cb51d05ba
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-10-20 22:39:26 +0300

    Update isCompleted

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import TodoVO from './src/model/vos/TodoVO.js';
+import { disableButtonWhenTextInvalid } from '@/utils/domUtils.js';
+import { isStringNotNumberAndNotEmpty } from '@/utils/stringUtils.js';
+import { localStorageListOf, localStorageSaveListOfWithKey } from '@/utils/databaseUtils.js';
+import TodoView from '@/view/TodoView.js';
 
 const domInpTodoTitle = document.getElementById('inpTodoTitle');
 const domBtnCreateTodo = document.getElementById('btnCreateTodo');
@@ -6,23 +10,31 @@ const domListOfTodos = document.getElementById('listOfTodos');
 
 domBtnCreateTodo.addEventListener('click', onBtnCreateTodoClick);
 domInpTodoTitle.addEventListener('keyup', onInpTodoTitleKeyup);
+domListOfTodos.addEventListener('change', onTodoListChange);
 
 const LOCAL_LIST_OF_TODOS = 'listOfTodos';
+const LOCAL_INPUT_TEXT = 'inputText';
 
 const listOfTodos = localStorageListOf(LOCAL_LIST_OF_TODOS);
 
 console.log('> Initial value -> listOfTodos', listOfTodos);
 
+domInpTodoTitle.value = localStorage.getItem(LOCAL_INPUT_TEXT);
 renderTodoListInContainer(listOfTodos, domListOfTodos);
-disableButtonWhenTextInvalid(
-  domBtnCreateTodo,
-  domInpTodoTitle.value,
-  isStringNotNumberAndNotEmpty,
-  {
-    textWhenEnabled: 'Create',
-    textWhenDisabled: 'Enter text',
+disableButtonWhenTextInvalid(domBtnCreateTodo, domInpTodoTitle.value, isStringNotNumberAndNotEmpty);
+
+function onTodoListChange(event) {
+  console.log('> onTodoListChange -> event:', event);
+  const target = event.target;
+  const index = target.id;
+  if (index && typeof index === 'string') {
+    const indexInt = parseInt(index.trim());
+    const todoVO = listOfTodos[indexInt];
+    console.log('> onTodoListChange -> todoVO:', indexInt, todoVO);
+    todoVO.isCompleted = !!target.checked;
+    saveListOfTodo();
   }
-);
+}
 
 function onBtnCreateTodoClick() {
   // console.log('> domBtnCreateTodo -> handle(click)', event);
@@ -30,8 +42,9 @@ function onBtnCreateTodoClick() {
   // console.log('> domBtnCreateTodo -> todoInputTitleValue:', todoTitleValueFromDomInput);
   if (isStringNotNumberAndNotEmpty(todoTitleValueFromDomInput)) {
     listOfTodos.push(TodoVO.createFromTitle(todoTitleValueFromDomInput));
-    localStorage.setItem(LOCAL_LIST_OF_TODOS, JSON.stringify(listOfTodos));
+    saveListOfTodo();
     renderTodoListInContainer(listOfTodos, domListOfTodos);
+    domInpTodoTitle.value = '';
   }
 }
 
@@ -39,63 +52,20 @@ function onInpTodoTitleKeyup(event) {
   // console.log('> onInpTodoTitleKeyup:', event);
   const inputValue = event.currentTarget.value;
   // console.log('> onInpTodoTitleKeyup:', inputValue);
-  disableButtonWhenTextInvalid(
-    domBtnCreateTodo,
-    inputValue,
-    isStringNotNumberAndNotEmpty,
-    {
-      textWhenEnabled: 'Create',
-      textWhenDisabled: 'Enter text',
-    }
-  );
+  disableButtonWhenTextInvalid(domBtnCreateTodo, inputValue, isStringNotNumberAndNotEmpty);
+  localStorage.setItem(LOCAL_INPUT_TEXT, inputValue);
 }
 
-function isStringNotNumberAndNotEmpty(value) {
-  const isValueString = typeof value === 'string';
-  const isValueNotNumber = isNaN(parseInt(value));
-
-  const result = isValueString && isValueNotNumber && value.length > 0;
-
-  console.log('> isStringNotNumberAndNotEmpty -> result', {
-    result,
-    isInputValueString: isValueString,
-    isInputValeNotNumber: isValueNotNumber,
-  });
-  return result;
-}
-
-function localStorageListOf(key) {
-  const value = localStorage.getItem(key);
-  console.log('> localStorageListOf: value =', value);
-  if (value === null) return [];
-  const parsedValue = JSON.parse(value);
-  const isParsedValueArray = Array.isArray(parsedValue);
-  return isParsedValueArray ? parsedValue : [];
-}
-
-function disableButtonWhenTextInvalid(
-  button,
-  text,
-  validateTextFunction,
-  { textWhenDisabled, textWhenEnabled } = {}
-) {
-  if (!validateTextFunction) throw new Error('Validate method must be defined');
-
-  if (validateTextFunction(text)) {
-    button.disabled = false;
-    if (textWhenEnabled) button.textContent = textWhenEnabled;
-  } else {
-    button.disabled = true;
-    if (textWhenDisabled) button.textContent = textWhenDisabled;
-  }
-}
-
-function renderTodoListInContainer(list, container) {
+function renderTodoListInContainer(listOfTodoVO, container) {
   let output = '';
-  let item;
-  for (let index in list) {
-    item = list[index];
-    output += `<li>${item.title}</li>`;
+  let todoVO;
+  for (let index in listOfTodoVO) {
+    todoVO = listOfTodoVO[index];
+    output += TodoView.createSimpleViewFromVO(index, todoVO);
   }
   container.innerHTML = output;
+}
+
+function saveListOfTodo() {
+  localStorageSaveListOfWithKey(LOCAL_LIST_OF_TODOS, listOfTodos);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 // vite.config.js
 /** @type {import('vite').UserConfig} */
-import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
+import path from 'path';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
   // omit
-  plugins: [mkcert()],
+  plugins: [],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   server: {
     port: 8089,
-    cors: true,
-    host: "local.dev",
-    https: true,
+    // cors: true,
+    // host: 'local.dev',
+    // https: true,
   },
 });
