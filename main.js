0fc8ecca9857a694f22d9877b7641e29e244e362
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-10-27 19:12:41 +0300

    Functional way

--- main.js
+++ main.js
@@ -5,9 +5,19 @@ const domFullNameContainer = document.getElementById('fullNameContainer');
 domInputFirstName.addEventListener('keyup', onHandleInputs);
 domInputLastName.addEventListener('keyup', onHandleInputs);
 
+const inputs = [domInputFirstName, domInputLastName];
+
 function onHandleInputs() {
-  const fistName = domInputFirstName.value;
-  const lastName = domInputLastName.value;
-  console.log('> onHandleInputs:', { fistName, lastName });
-  domFullNameContainer.innerText = fistName + ' ' + lastName;
+  // const fistName = domInputFirstName.value;
+  // const lastName = domInputLastName.value;
+  // console.log('> onHandleInputs:', { fistName, lastName });
+  // domFullNameContainer.innerText = fistName + ' ' + lastName;
+  domFullNameContainer.innerText = joinListItemsContainsValue(inputs, ' ');
+}
+
+function joinListItemsContainsValue(list, delimiter) {
+  return list
+    .filter(({ value }) => !!value)
+    .map(({ value }) => value)
+    .join(delimiter);
 }
